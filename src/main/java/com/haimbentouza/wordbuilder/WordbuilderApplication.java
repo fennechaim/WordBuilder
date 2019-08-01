5603b573f799d7a669d7b4dcec4583efbbeb142d
Haim Bentouza <fennechaim@gmail.com>
2019-08-01 10:14:32 +0200

    first commit

---
+++ src/main/java/com/haimbentouza/wordbuilder/WordbuilderApplication.java
@@ -0,0 +1,13 @@
+package com.haimbentouza.wordbuilder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WordbuilderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WordbuilderApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/haimbentouza/wordbuilder/WordbuilderApplicationTests.java
@@ -0,0 +1,16 @@
+package com.haimbentouza.wordbuilder;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class WordbuilderApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
